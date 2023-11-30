9fefad5ecbeaa61370b1506bc2a02089fabe2b91
Василь Кочержук <vasya98812018@outlook.com>
2023-11-30 20:48:53 +0200

    3 changes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,12 @@
 new Swiper(".swiper-container", {
   spaceBetween: 40,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    // hide: true,
   },
 });
