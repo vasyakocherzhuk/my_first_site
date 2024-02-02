06b5e0df6a1d28a39ff6c61b485d7272e89c51e8
Василь Кочержук <vasya98812018@outlook.com>
2024-02-02 11:03:13 +0200

    commit

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,78 @@
-new Swiper(".swiper-container", {
-  spaceBetween: 40,
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  //   direction: "",
   loop: true,
+  // slidesPerView: 'auto',
+  keyboard: {
+    enabled: true,
+  },
+  // mousewheel: true,
+  // autoplay: {
+  //   enabled: true,
+  //   delay: 2000,
+  // },
+
+  slidesPerView: 1,
+  spaceBetween: 40,
+
+  breakpoints: {
+    850: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    1200: {
+      slidesPerView: 3,
+      loop: false,
+    },
+  },
+
+  // If we need pagination
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+
+  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
     // hide: true,
+    draggable: true,
+    // dragSize: 300,
   },
 });
+
+// const sliderContainer = document.querySelector(".swiper-container");
+// const desktopPortfolio = document.querySelector(".desktop-portfolio");
+// console.log(sliderContainer);
+
+// const hiddenSlider = () => {
+//   // console.log(window.innerWidth);
+//   if (window.innerWidth >= 1200) {
+//     sliderContainer.classList.add("visually-hidden");
+//   } else {
+//     desktopPortfolio.classList.add("visually-hidden");
+//   }
+// };
+
+// window.addEventListener("load", hiddenSlider);
+
+// const hiddenSlider = () => {
+//   if (window.innerWidth > 1200) {
+//     sliderContainer.classList.add("hiddenPortfolio");
+//     desktopPortfolio.classList.remove("hiddenPortfolio");
+//     // sliderContainer.style.display = "none";
+//     // desktopPortfolio.style.display = "flex";
+//   } else {
+//     sliderContainer.classList.remove("hiddenPortfolio");
+//     desktopPortfolio.classList.add("hiddenPortfolio");
+//     // sliderContainer.style.display = "flex";
+//     // desktopPortfolio.style.display = "none";
+//   }
+// };
+
+// window.addEventListener("resize", hiddenSlider);
