048c1c12d39a96f6139a02261df45e5bed24aebc
vasyakocherzhuk <148870254+vasyakocherzhuk@users.noreply.github.com>
2024-02-08 10:28:16 +0000

    Deploying to gh-pages from @ vasyakocherzhuk/my_first_site@577f5bd54b3f640612a31cc7ce9dd8a8b22dfd1f 🚀

---
+++ projects.ac1fbda3.js
@@ -0,0 +1,2 @@
+var navLinks=document.querySelectorAll(".navLink");navLinks.forEach((function(i){i.classList.remove("active"),window.location.href.includes(i.id)&&i.id&&i.classList.add("active")}));
+//# sourceMappingURL=projects.ac1fbda3.js.map
